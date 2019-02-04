7de0624c4b57ad17c4a58ef2d48abd1f5a306f9b
nadavgiladi <46893005+nadavgiladi@users.noreply.github.com>
2019-02-04 13:42:04 +0200

    Delete data_dict so far.py

--- data_dict so far.py
+++
@@ -1,8 +0,0 @@
-# x is [4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-# dx is [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-# y is [11.5, 12.9, 15.1, 17.5, 18.7, 21.0, 23.4]
-# dy is [0.2, 0.3, 0.1, 0.3, 0.5, 0.1, 0.4]
-# x axis: is time[sec]
-# y axis: is velocity[m/sec]
-#
-# Process finished with exit code 0
